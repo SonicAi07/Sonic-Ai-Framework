810d5b9a7bc70a1b84af1494f47a2c4a8921793d
SonicAi07 <81548276+SonicAi07@users.noreply.github.com>
2021-04-20 19:30:59 +0530

    Add files via upload

--- Fashion_house.js
+++ Fashion_house.js
@@ -12,6 +12,13 @@ function order() {
 
     output = 'Your order will be delivered to your address soon....'
     alert(output);
+    window.location.href="https://sonicai07.github.io/Fashion-House/home.html";
     return false;
-    window.location.href="";
+    
+}
+
+function hello() {
+    alert('Rgistration Successful- Continue Shopping');
+    window.location.href="https://sonicai07.github.io/Fashion-House/home.html";
+    return false;
 }
